a02a7ca0c7811971d222c9aa4a9796cda3f4eb60
divineion <najima.guermoudi@gmail.com>
2023-08-24 11:55:38 +0200

    fix/main: correction d'une erreur de sélecteur dans forecast-5days.js

--- src/js/forecast-5days.js
+++ src/js/forecast-5days.js
@@ -3,6 +3,8 @@ import { nomVilleAPI } from "./app.js";
 import { nomVilleAPIRandom } from "./random-city.js";
 import { kelvinToCelsius } from "./app.js";
 
+let weatherIcon;
+
 export function forecast() {
   const villeAPI = nomVilleAPI || nomVilleAPIRandom;
   const forecastContainer = document.querySelector(".forecast5days");
@@ -12,8 +14,8 @@ export function forecast() {
     .then((response) => response.json())
     .then((data) => {
 
-      const forecast5days = document.createElement("div");
-      forecast5days.classList.add("forecast5days");
+      const forecast5days = document.createElement("div")
+      forecast5days.classList.add("forecast5days")
 
       //tableau contenant les prévisions à 9h de jour J à J+4
       const nextDaysForecast = [
@@ -21,31 +23,51 @@ export function forecast() {
         data.list[15],
         data.list[31],
         data.list[23],
-        data.list[39],
-      ];
+        data.list[39]
+      ]
 
-      const main = document.querySelector('main');
-      const title = document.createElement('div');
-      title.classList.add('forecast5days__title');
-      title.textContent = villeAPI;
-      forecast5days.appendChild(title);
+      const main = document.querySelector('main')
+      const title = document.createElement('div')
+      title.classList.add('forecast5days__title')
+      title.textContent = villeAPI
+      forecast5days.appendChild(title)
 
       //création des div pour chaque jour
       nextDaysForecast.forEach((element) => {
-        let forecastPerDay = document.createElement("div");
-        let forecastDate = document.createElement("div");
-        let forecastTemp = document.createElement("div");
-        let forecastWeather = document.createElement("div");
-        forecastPerDay.classList.add("forecast5days__forecastPerDay");
+        let forecastPerDay = document.createElement("div")
+        let forecastDate = document.createElement("div")
+        let forecastTemp = document.createElement("div")
+        let forecastWeather = document.createElement("div")
+        forecastPerDay.classList.add("forecast5days__forecastPerDay")
         forecastDate.classList.add(
           "forecast5days__forecastPerDay--forecastDate"
-        );
+        )
         forecastTemp.classList.add(
           "forecast5days__forecastPerDay--forecastTemp"
-        );
+        )
         forecastWeather.classList.add(
           "forecast5days__forecastPerDay--forecastWeather"
-        );
+        )
+        forecastWeather.classList.add(
+          "weather-icon"
+        )
+        
+        switch (element.weather[0].main) {
+          case "Clear":
+            forecastWeather.classList.add('clear');
+            break;
+          case "Clouds":
+            forecastWeather.classList.add("clouds");
+            break;
+          case "Rain":
+            forecastWeather.classList.add("rain");
+            break;
+          case "Snow":
+            forecastWeather.classList.add("snow");
+            break;
+        }
+console.log(element.weather[0].main)
+
         forecastPerDay.appendChild(forecastDate);
         forecastPerDay.appendChild(forecastTemp);
         forecastPerDay.appendChild(forecastWeather);
